c3fd464ddf20bde15e17b77c01038374027c3cc4
Francky <franciskipchumba5@gmail.com>
2020-02-21 10:08:37 +0300

    (feat): implement react

---
+++ pocketapp/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// local imports
+import Approutes from './Routes';
+import './static/App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      token: '',
+
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {/* <Navigation /> */}
+          <Approutes />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App
